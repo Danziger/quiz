0fe1dc0865d0273ef1ec3ce04deff7aa34a7fba0
Dani Gámez Franco <gmzcodes@gmail.com>
2015-07-18 13:14:38 +0200

    Desplegar en Heroku con Postgres

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var routes = require('./routes/index');
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,7 +23,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
