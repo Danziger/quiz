65dd98255c02478830f95beb035e6f3f9a8f801c
Dani Gámez Franco <gmzcodes@gmail.com>
2015-07-24 12:53:21 +0200

    Improved mobile UX

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+var menu = null;
+var button = null;
+
+// This event may need a fallback:
+// http://stackoverflow.com/questions/9899372/pure-javascript-equivalent-to-jquerys-ready-how-to-call-a-function-when-the
+document.addEventListener('DOMContentLoaded', function() {
+	
+	console.log("LOAD");
+	
+	menu = document.getElementById("menu");
+	button = document.getElementById("search-menu-button");
+	
+	document.onclick = function(e) {
+	
+		console.log(e.target);
+	
+		if(e.target === button)
+			menu.classList.toggle("visible");
+		else
+			menu.classList.remove("visible");
+	}
+
+}, false);
