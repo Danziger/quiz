93de9771c63a47b54998bad0f9ec2ecf86559503
Dani Gámez Franco <gmzcodes@gmail.com>
2015-08-10 23:44:58 +0200

    Fixed the session storage options for Heroku.

--- app.js
+++ app.js
@@ -38,12 +38,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); // extended = true by default
 app.use(cookieParser("this-is-the-seed-for-the-cookies"));
-app.use(session({
+app.use(session(/*{
 	secret: "this-is-the-seed-for-the-cookies",
 	resave: false,
 	saveUninitialized: true,
 	cookie: { secure: true }
-}));
+}*/));
 
 // Dinamyc helpers:
 app.use(function(req, res, next) {
